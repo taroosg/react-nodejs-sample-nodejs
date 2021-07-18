7badc214415fca884991dac7dc0d14d8a6a25c2f
taroosg <root@taroosg.dev>
2021-07-18 22:30:16 +0900

    init

---
+++ routes/janken.route.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { getResult } from "../controllers/janken.controller.js";
+
+export const jankenRouter = express.Router();
+
+jankenRouter.post("/", (req, res) => getResult(req, res));
+
